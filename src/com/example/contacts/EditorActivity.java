9d6a33d065f3829e73891fb59e7d2e4fef14d319
AlexHRG <hirurg_03@ukr.net>
2014-10-06 11:28:42 +0300

    added strings, list layout and blank second activity

--- src/com/example/contacts/EditorActivity.java
+++ src/com/example/contacts/EditorActivity.java
@@ -1,23 +1,22 @@
 package com.example.contacts;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class EditorActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_editor);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.editor, menu);
 		return true;
 	}
 
@@ -27,9 +26,8 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.add_contact) {
-			Intent intent = new Intent(this, EditorActivity.class);
-			startActivity(intent);
+		if (id == R.id.action_settings) {
+			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
